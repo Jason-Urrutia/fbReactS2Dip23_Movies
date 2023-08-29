1f7f6f5f284934bf72c4a4bcd8462012416c8bef
Jason Urrutia <140996171+urrutij@users.noreply.github.com>
2023-08-29 21:05:10 +1000

    Adding Firebase

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyAuKNcMfmSPc5FzxJJA2cDGQPO3MTN2c6I",
+    authDomain: "class-project-cddd1.firebaseapp.com",
+    projectId: "class-project-cddd1",
+    storageBucket: "class-project-cddd1.appspot.com",
+    messagingSenderId: "46889756479",
+    appId: "1:46889756479:web:0bd50f706926efc581d58a" 
+}
