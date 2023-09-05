b1d9da292f8f561a8c26dea4aca693aae5d6de1b
Jason Urrutia <140996171+urrutij@users.noreply.github.com>
2023-09-05 20:10:27 +1000

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import {FirebaseConfig} from "./config/Config"
 import {initializeApp} from "firebase/app"
+import { Routes, Route } from "react-router-dom"
 
-import Container from "react-bootstrap/Container"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
+import {Header } from "./components/Header"
 import './App.css';
+import { About } from "./pages/About"
+import { Home } from "./pages/Home"
+import { Contact } from "./pages/Contact"
 
 function App() {
   const FBapp = initializeApp(FirebaseConfig)
   return (
     <div className="App">
-      <Navbar>
-        <Container>
-          <Navbar.Brand>App</Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <h1>React app</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Home greeting="Hey, You're on the Home page!" /> } />
+        <Route path="/about" element={ <About greeting="Hey you, this is the About page!" /> } />
+        <Route path="/contact" element={ <Contact /> } />
+      </Routes>
     </div>
   );
 }
