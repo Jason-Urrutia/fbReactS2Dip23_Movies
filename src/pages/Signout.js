04bc90b975497ba48281a2e32337d3749d71d315
Jason Urrutia <140996171+urrutij@users.noreply.github.com>
2023-09-19 19:48:56 +1000

    adding signout component

--- src/pages/Signout.js
+++ src/pages/Signout.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 
 export function Signout( props ) {
