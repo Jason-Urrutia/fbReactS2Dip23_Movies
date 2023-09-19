1fb4778c4484fcb4d0deda0fd60502a8e6a5efc7
Jason Urrutia <140996171+urrutij@users.noreply.github.com>
2023-09-19 19:47:59 +1000

    adding signin component

---
+++ src/pages/Signout.js
@@ -0,0 +1,26 @@
+import { useEffect } from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import { useNavigate } from "react-router-dom";
+
+
+export function Signout( props ) {
+    
+    const nav = useNavigate()
+    
+    useEffect(() => {
+        props.handler()
+        nav("/")
+    })
+
+    return(
+        <Container>
+            <Row>
+                <Col>
+                {/* Signout */}
+                </Col>
+                </Row>
+        </Container>
+    )
+}
