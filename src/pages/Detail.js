25ceaa19872c382ad9e27400707992c39fc439b2
Jason Urrutia <140996171+urrutij@users.noreply.github.com>
2023-11-21 21:25:19 +1100

    review form component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,11 @@
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import Form from "react-bootstrap/Form"
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { ItemImage } from "../components/ItemImage"
-import Button from "react-bootstrap/Button"
+import { ReviewForm } from "../components/ReviewForm"
+
 
 export function Detail(props) {
     const [movieData, setMovieData] = useState()
@@ -27,10 +27,10 @@ export function Detail(props) {
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={6}>
+                    <Col md={4}>
                         <ItemImage source={movieData.cover_image} />
                     </Col>
-                    <Col md={6}>
+                    <Col md={8}>
                         <h2>More Information</h2>
                         <h3>Storyline</h3>
                         <p>{movieData.storyline}</p>
@@ -48,32 +48,12 @@ export function Detail(props) {
                         <p>{movieData.runtime}</p>
                         <h3>IMDB Link</h3>
                         <p>{movieData.imdb_link}</p>
-                        <Form>
-                            <h3>Review this movie</h3>
-                            <Form.Group>
-                                <Form.Label>Star Rating</Form.Label>
-                                <Form.Select>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                    <option value="5">5</option>
-                                </Form.Select>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Review Title</Form.Label>
-                                <Form.Control type="text" placeholder="I love this movie" />
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label>Review</Form.Label>
-                                <Form.Control as="textarea" rows={4} cols={30} placeholder="I could not stop watching!" />
-                            </Form.Group>
-                            <Button type="submit" variant="primary">Submit</Button>
-                        </Form>
                     </Col>
                 </Row>
                 <Row>
-                    <Col></Col>
+                    <Col>
+                    <ReviewForm movietitle={movieData.title} />
+                    </Col>
                 </Row>
             </Container>
         )
